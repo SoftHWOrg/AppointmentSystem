23c833bd8323acbe856bcca173636b3dd5232479
خالد جمال سعد زاده <144535771+KLDZ1@users.noreply.github.com>
2026-05-09 21:36:45 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -24,7 +24,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    
 
         TxtUserRepository userRepo = new TxtUserRepository();
         TxtAppointmentRepository apptRepo = new TxtAppointmentRepository();
